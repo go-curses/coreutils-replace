03ab8958a56ef1be20b6fbf239ba1826a78487b6
Kevin C. Krinke <kevin@krinke.ca>
2022-09-05 15:14:02 -0400

    replace: updated version to 0.2.2

--- cmd/rpl/main.go
+++ cmd/rpl/main.go
@@ -17,7 +17,7 @@ const (
 	APP_NAME    = "rpl"
 	APP_USAGE   = "search and replace"
 	APP_DESC    = "command line search and replace"
-	APP_VERSION = "0.2.1"
+	APP_VERSION = "0.2.2"
 	APP_TAG     = "rpl"
 	APP_TITLE   = "rpl"
 )
